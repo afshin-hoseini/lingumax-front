536dcb83266b2bb738c1d751d7aeac01a11725be
Afshin <afshin.hoseini@gmail.com>
2021-01-19 12:40:21 +0330

    Updated password

--- src/pages/meeting/Room.tsx
+++ src/pages/meeting/Room.tsx
@@ -188,8 +188,8 @@ const useRTC = (peerSocketId?: string)=> {
                 {urls: ["stun:stun.services.mozilla.com", "stun:numb.viagenie.ca"]},
                 {
                     urls: ["turn:51.75.233.33"],
-                    username:"1610463185",
-                    credential:"d7Zst4QKhI9XaT4t0eR3llcNd/4="
+                    username:"1613047227",
+                    credential:"0E2cmaQfz9tQ2UcYr/U9JLM0GG0="
                 },
              ]
         });
